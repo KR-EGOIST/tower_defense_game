694c90f62fd27c0eb4a3925f45a6c06fbeea48c8
diddntjd99 <diddntjd99@gmail.com>
2024-06-17 18:03:42 +0900

    🎉 add : 몬스터 kill 시 이벤트 호출 및 점수 +

--- src/handlers/handlerMapping.js
+++ src/handlers/handlerMapping.js
@@ -1,6 +1,9 @@
+import { monsterKillHandler } from './monster.handler.js';
+
 const handlerMappings = {
   // 1: 함수 이름,
   // 이런 식으로 사용하시면 됩니다.
+  21: monsterKillHandler,
 };
 
 export default handlerMappings;
