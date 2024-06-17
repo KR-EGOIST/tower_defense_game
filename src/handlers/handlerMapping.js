8dcf25c78507e5f6c71ef26aa7fcc41a508ac74c
diddntjd99 <diddntjd99@gmail.com>
2024-06-17 12:12:27 +0900

    🎉 add : Handler Mapping 파일 생성

---
+++ src/handlers/handlerMapping.js
@@ -0,0 +1,6 @@
+const handlerMappings = {
+    // 1: 함수 이름,
+    // 이런 식으로 사용하시면 됩니다.
+  };
+  
+  export default handlerMappings;
--- src/handlers/helper.js
+++ src/handlers/helper.js
@@ -5,3 +5,33 @@ export const handleDisconnect = (socket, uuid) => {
   console.log(`User disconnected: ${socket.id}`);
   console.log('Current users:', getUsers());
 };
+
+export const handleConnection = (socket, userUUID) => {
+  console.log(`New user connected: ${userUUID} with socket ID ${socket.id}`);
+  console.log('Current users:', getUsers());
+
+  socket.emit('connection', { uuid: userUUID });
+};
+
+export const handleEvent = (io, socket, data) => {
+  if (!CLIENT_VERSION.includes(data.clientVersion)) {
+    socket.emit('response', { status: 'fail', message: 'Client version mismatch' });
+    return;
+  }
+
+  const handler = handlerMappings[data.handlerId];
+  if (!handler) {
+    socket.emit('response', { status: 'fail', message: 'Handler not found' });
+    return;
+  }
+
+  // 적절한 핸들러에 userID 와 payload를 전달하고 결과를 받습니다.
+  const response = handler(data.userId, data.payload);
+  // 만약 결과에 broadcast (모든 유저에게 전달)이 있다면 broadcast 합니다.
+  if (response.broadcast) {
+    io.emit('response', response);
+    return;
+  }
+  // 해당 유저에게 적절한 response를 전달합니다.
+  socket.emit('response', response);
+};
