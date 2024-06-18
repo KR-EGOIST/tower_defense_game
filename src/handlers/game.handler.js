e56746daf9c7e363e02e651e5ac2ac6dcbdcbbb0
KR-EGOIST <jhso5041@naver.com>
2024-06-18 14:46:23 +0900

    🩹 fix : 필요없는 콘솔 제거

--- src/handlers/game.handler.js
+++ src/handlers/game.handler.js
@@ -8,9 +8,6 @@ export const gameStart = (uuid, payload) => {
 // 게임 종료 시 작동하는 핸들러
 export const gameEnd = async (uuid, payload) => {
   const currentScore = await getScore(payload);
-  console.log(currentScore);
-  console.log(payload.score);
-  console.log(payload);
   // 현재 점수가 DB의 점수보다 높으면 DB의 점수를 현재 점수로 업데이트
   if (currentScore < payload.score) {
     await setScore(payload);
