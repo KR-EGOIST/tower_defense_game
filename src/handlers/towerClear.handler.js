df8b141b4e4a099213f37ca67376a7cef68bb79a
diddntjd99 <diddntjd99@gmail.com>
2024-06-18 12:06:14 +0900

    ♻️ refactor : tower 코드들 tower handler에서 관리

--- src/handlers/towerClear.handler.js
+++
@@ -1,7 +0,0 @@
-import { clearTower } from '../models/tower.model.js';
-
-//서버에 저장된 타워들을 전부 삭제한다.
-export const towerClearHandler = () => {
-  clearTower();
-  return { status: 'success', message: 'Tower Clear' };
-};
